dbebb6df338c74f24a999c0d584c0d46bcec2272
王海峰 <whiflyer@gmail.com>
2019-09-04 17:59:34 +0800

    V1.2

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <FL/Fl.H>
 #include <FL/Fl_Ask.H>
 #include <FL/x.H>   // needed for fl_display
+#pragma comment( linker, "/subsystem:/"windows/" /entry:/"mainCRTStartup/"" ) // 设置入口地址
 #include "windows.h"
 #include "icon.h"
 
@@ -13,13 +14,64 @@
 
 SharePara g_share_para;   //share param
 
+/*!
+system() to hide command window
+
+    Input:  cmd -- command line
+            cnt -- size of output buffer in btes
+    Output: out -- command output buffer
+    Return: true=success, false=failure
+*/
+bool system_hide(char *out, int cnt, const char *cmd)
+{
+    SECURITY_ATTRIBUTES sa;
+    sa.nLength   =   sizeof(SECURITY_ATTRIBUTES);
+    sa.lpSecurityDescriptor   =   NULL;
+    sa.bInheritHandle   =   TRUE;
+
+    HANDLE hRead, hWrite;
+    if (!CreatePipe(&hRead, &hWrite, &sa, 0)) {
+        return false;
+    }
+
+    STARTUPINFO si;
+    si.cb = sizeof(STARTUPINFO);
+    GetStartupInfo(&si);
+    si.hStdError = hWrite;
+    si.hStdOutput = hWrite;
+    si.wShowWindow = SW_HIDE;
+    si.dwFlags = STARTF_USESHOWWINDOW | STARTF_USESTDHANDLES;
+    PROCESS_INFORMATION pi;
+    char cmd_buf[256];
+    strncpy(cmd_buf, cmd, 256);
+    printf("_debug_:%d@%s.\n", __LINE__, __FUNCTION__);
+    if (!CreateProcess(NULL, cmd_buf, NULL, NULL, true, 0, NULL, NULL, &si, &pi)) {
+        return false;
+    }
+    CloseHandle(hWrite);
+
+    if (out) {
+        DWORD bytesRead;
+        memset(out, 0, cnt);
+        ReadFile(hRead, out, cnt-1, &bytesRead, NULL);
+    }
+    return true;
+}
+
 int main(int argc, char**argv)
 {
-    uint32_t ver = system("up_pqied.exe -v > nul");
     char title[32];
+    /*uint32_t ver = system("up_pqied.exe -v > nul");
     sprintf(title, "up_pqied:%d.%d.%d GUI:%d.%d", ver>>22, (ver>>10)&0x3ff, ver&0x3ff, _VERSION_MAJOR, _VERSION_MINOR);
-    //sprintf(title, "up_pqied:3.26.28 GUI:4.56.78");
-    
+    */
+    char obuf[64];
+    int ver[3];
+    if (system_hide(obuf, 64, "up_pqied.exe -v")) {
+        printf("%s\n", obuf);
+        sscanf(obuf, "up_pqied %d.%d.%d", &ver[0], &ver[1], &ver[2]);
+    }
+    sprintf(title, "up_pqied:%d.%d.%d GUI:%d.%d", ver[0], ver[1], ver[2], _VERSION_MAJOR, _VERSION_MINOR);
+
     MainWindow *window = new MainWindow(340, 226, title);
     window->end();
     g_share_para.update = false;
@@ -30,7 +82,7 @@ int main(int argc, char**argv)
     window->show(argc, argv);
     Fl::add_idle(CBIdle, window);
     //KeyFileCreate();
-    
+
     //return Fl::run();
     while(Fl::wait()) {
         Sleep(50);  //sleep for 50ms
@@ -72,7 +124,7 @@ void CBMainWindow(Fl_Widget * wdg, void * data)
         para->update = true;
     }
     ParaFileSave(para);
-    
+
     //KeyFileClean();
     exit(0);
 }
@@ -84,7 +136,7 @@ void ParaFileRead(SharePara *para)
     strcpy(para->ip, "192.168.1.100");
     strcpy(para->port, "12307");
     para->reboot = 1;
-    
+
     FILE *fp;
     const char *file_name = kCfgFile;
     if ((fp = fopen(file_name, "r"))==NULL) {
@@ -100,45 +152,59 @@ void ParaFileRead(SharePara *para)
             //printf("name=%s, val=%s\n", name, val);
             if (!(scan&1)) {
                 if ( !strcmp(name, "ip") ) {
-                    strcpy(para->ip, val); scan |= 1; break;
+                    strcpy(para->ip, val);
+                    scan |= 1;
+                    break;
                 }
             }
             if (!(scan&(1<<1))) {
                 if ( !strcmp(name, "port") ) {
-                    strcpy(para->port, val); scan |= (1<<1); break;
+                    strcpy(para->port, val);
+                    scan |= (1<<1);
+                    break;
                 }
             }
             if (!(scan&(1<<2))) {
                 if ( !strcmp(name, "typeidx") ) {
-                    para->type.val = atoi(val); scan |= (1<<2); break;
+                    para->type.val = atoi(val);
+                    scan |= (1<<2);
+                    break;
                 }
             }
             if (!(scan&(1<<3))) {
                 if ( !strcmp(name, "prog") ) {
                     sscanf(val, "%x", &para->prog.val);
-                    scan |= (1<<3); break;
+                    scan |= (1<<3);
+                    break;
                 }
             }
             if (!(scan&(1<<4))) {
                 if ( !strcmp(name, "force") ) {
-                    para->force = atoi(val); scan |= (1<<4); break;
+                    para->force = atoi(val);
+                    scan |= (1<<4);
+                    break;
                 }
             }
             if (!(scan&(1<<5))) {
                 if ( !strcmp(name, "cst61850") ) {
-                    para->t61850.val = atoi(val)-1; scan |= (1<<5); break;
+                    para->t61850.val = atoi(val)-1;
+                    scan |= (1<<5);
+                    break;
                 }
             }
             if (!(scan&(1<<6))) {
                 if ( !strcmp(name, "reboot") ) {
-                    para->reboot = atoi(val); scan |= (1<<6); break;
+                    para->reboot = atoi(val);
+                    scan |= (1<<6);
+                    break;
                 }
             }
             if (!(scan&(1<<7))) {
                 if ( !strcmp(name, "vendor") ) {
                     para->vendor.val = atoi(val);
                     if (para->vendor.val) para->vendor.val -= 1;
-                    scan |= (1<<7); break;
+                    scan |= (1<<7);
+                    break;
                 }
             }
         } while(0);
@@ -148,14 +214,15 @@ void ParaFileRead(SharePara *para)
 
 /*!
     Input:  idx -- typeidx
-    Return: type 
+    Return: type
 */
 int TypeIdx2Type(int idx)
 {
     const char * file_name = ".sys/script/type_map.lst";
     FILE *fp;
     if ((fp = fopen(file_name, "r"))==NULL) {
-        printf("Can't open %s\n", file_name); return -2;
+        printf("Can't open %s\n", file_name);
+        return -2;
     }
 
     char stri[64];
@@ -183,7 +250,7 @@ void ParaFileSave(SharePara *para)
     if ((fp = fopen(file_name, "w"))==NULL) {
         return;
     }
-    
+
     fprintf(fp, "ip = %s\n", para->ip);
     fprintf(fp, "port = %s\n", para->port);
     fprintf(fp, "typeidx = %d\n", para->type.val);
@@ -201,11 +268,11 @@ void KeyFileCreate()
 {
     FILE *f_strm;
     const char *file_name = kKeyFile;
-    
+
     f_strm = fopen(file_name, "w");
     fwrite(private_key, strlen(private_key), 1, f_strm);
     fclose(f_strm);
-    
+
     file_name = kRespYesFile;
     f_strm = fopen(file_name, "w");
     fwrite("y", 1, 1, f_strm);
@@ -223,3 +290,4 @@ void KeyFileClean()
     remove(kRespYesFile);
     //remove(kRespNoFile);
 }
+
